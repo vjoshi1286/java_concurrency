e4338ac4900f1bff540eeaf6a9c45b7bd04b564e
vaibhavJ <vjoshi1286@gmail.com>
2023-08-20 17:14:10 +0530

    Pushing some corrections

--- NoVisibility.java
+++ NoVisibility.java
@@ -1,21 +1,21 @@
-public class Test{
-    
+public class NoVisibility {
+
     private static boolean ready;
     private static int val;
 
     private static class ReaderThread extends Thread {
         @Override
         public void run() {
-            while (!ready);
-            
-		System.out.println(val + " " + Thread.currentThread()
+            while (!ready) ;
+
+            System.out.println(val + " " + Thread.currentThread()
                     .getName());
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         new ReaderThread().start();
-	Thread.sleep(4000);
+        Thread.sleep(4000);
         ready = true;
         val = 42;
     }
